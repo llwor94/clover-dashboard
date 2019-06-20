faa2b3dba9daf4230623b0ab357f38fe01b03e05
llwor94 <me@ldub.io>
2019-06-20 10:36:52 -0700

    🏎  basic apollo server outline

--- dashboard/server/src/index.js
+++ dashboard/server/src/index.js
@@ -1,1 +1,44 @@
 require('dotenv').config()
+const { mergeSchemas } = require('graphql-tools')
+const { ApolloServer } = require('apollo-server')
+
+const getSchema = require('./schema')
+
+const extendSchema = async () => {
+  const typeExtensions = ``
+  /* 
+     to do --> 
+        extend admin, tickets w/ answerhub data
+        create types-
+            ◦ spaces
+            ◦ user
+    */
+
+  const schemaExtensionResolvers = {
+    /*
+    to do -->
+  */
+  }
+  const remoteSchema = await getSchema()
+  const newSchema = mergeSchemas({
+    schemas: [remoteSchema, typeExtensions],
+    resolvers: schemaExtensionResolvers
+  })
+  return newSchema
+}
+
+const startServer = async () => {
+  const schema = await extendSchema()
+  const server = new ApolloServer({
+    schema
+  })
+  server.listen().then(({ url }) => {
+    console.log(`Server running at ${url}`)
+  })
+}
+
+try {
+  startServer()
+} catch (e) {
+  console.error(e)
+}
