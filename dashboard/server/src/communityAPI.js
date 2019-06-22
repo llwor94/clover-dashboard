be7294ce7e597530f0220510b322ead64b234461
llwor94 <me@ldub.io>
2019-06-21 22:49:12 -0700

    📝 defining schema

--- dashboard/server/src/communityAPI.js
+++ dashboard/server/src/communityAPI.js
@@ -1,9 +1,12 @@
 const axios = require('axios')
 
+const server = axios.create({
+    baseURL: `https://community.clover.com/services/v2/`
+})
+
 module.exports = {
-  getTickets: async () => {
-    /*
-        to do
-    */
+  getTickets: async ({spaceId}) => {
+    let tickets = await server({method: 'get', url: 'question.json', query: {answered: false, specialId}})
+    console.log(tickets)
   }
 }
