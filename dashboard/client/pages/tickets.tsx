7ce2005fd9f66606adb930504c1b65e40e38d33a
Frank Faustino <nawzyah@gmail.com>
2019-06-26 23:07:24 -0700

    🎨

--- dashboard/client/pages/tickets.tsx
+++ dashboard/client/pages/tickets.tsx
@@ -1,28 +1,30 @@
-import axios from 'axios'
 import { NextStatelessComponent } from 'next'
 import React, { useEffect, useState } from 'react'
 
 import Layout from '../components/Layout'
-import Ticket from '../components/Ticket'
-
 import Header from '../components/Header'
 import SubMenu from '../components/SubMenu'
+import Ticket from '../components/Ticket'
 
 import { getSpaces, getTickets } from '../gql/query'
 
 interface IProps {
   spaces: any[]
   tickets: any[]
+  query?: { space: string }
 }
 
-const Tickets: NextStatelessComponent<IProps> = ({ spaces, tickets: initalTickets, query }) => {
+const Tickets: NextStatelessComponent<IProps> = ({ spaces, tickets: initalTickets = [], query }) => {
   const [tickets, setTickets] = useState(initalTickets)
 
   useEffect(() => {
     const fetchTickets = async () => {
       if (query && query.space) {
         const { data } = await getTickets(query.space)
-        setTickets(data.tickets)
+
+        if (data && data.tickets) {
+          setTickets(data.tickets)
+        }
       }
     }
     fetchTickets()
@@ -30,11 +32,11 @@ const Tickets: NextStatelessComponent<IProps> = ({ spaces, tickets: initalTicket
 
   return (
     <Layout>
-      {spaces && <SubMenu spaces={spaces} space={query && query.space} />}
+      <SubMenu spaces={spaces} space={query && query.space} />
       <main className="main">
         <Header />
         <div className="content">
-          {tickets.map(t => (
+          {tickets && tickets.map(t => (
             <Ticket key={t.id} ticket={t} />
           ))}
         </div>
@@ -43,24 +45,11 @@ const Tickets: NextStatelessComponent<IProps> = ({ spaces, tickets: initalTicket
   )
 }
 
-Tickets.defaultProps = {
-  spaces: [],
-  tickets: []
-}
-
 Tickets.getInitialProps = async ({ query }) => {
   try {
-    const [
-      {
-        data: { spaces }
-      },
-      {
-        data: { tickets }
-      }
-    ] = await axios.all([getSpaces(), getTickets(query.space)])
+    const { data: { spaces } } = await getSpaces()
 
-    return { spaces, tickets, query }
-    // return { spaces, query }
+    return { spaces, query }
   } catch (e) {
     console.error(e.message)
   }
