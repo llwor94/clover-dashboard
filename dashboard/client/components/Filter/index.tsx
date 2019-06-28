d1daeacfd5d30ec13066791b71249e414d227776
Frank Faustino <nawzyah@gmail.com>
2019-06-27 17:16:00 -0700

    ✨ Filter component done

--- dashboard/client/components/Filter/index.tsx
+++ dashboard/client/components/Filter/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
 
+import Button from './Button'
+
 import './styles.scss'
 
-const Filter = () => <div className="filter">this is supposed to be the filter menu</div>
+const Filter = () => (
+  <div className="filter">
+    <Button name="No response" selected={true} />
+    <Button name="Last response by us" />
+    <Button name="All" />
+  </div>
+)
 
 export default Filter
