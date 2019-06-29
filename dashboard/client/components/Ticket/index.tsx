5d56357cc27072af3bb287047ed63bd209a29e92
Frank Faustino <nawzyah@gmail.com>
2019-06-29 04:16:29 -0700

    ⚡️ Add hover effect to Ticket

--- dashboard/client/components/Ticket/index.tsx
+++ dashboard/client/components/Ticket/index.tsx
@@ -1,45 +1,60 @@
 import moment from 'moment'
-import React from 'react'
+import React, { useState } from 'react'
+import { Transition } from 'react-transition-group'
 
 import EmptyCheckboxIcon from './EmptyCheckboxIcon'
 import StarIcon from './StarIcon'
 
 import './styles.scss'
 
-const Ticket = ({ ticket }) => (
-  <div className="ticket">
-    {/* aSB0aGluayBpJ20ga2luZGEgaW4gbG92ZSB3aXRoIHU= */}
-    {/* <div className="ticket__checkbox" /> */}
+const Ticket = ({ state, ticket, toggleState }) => (
+  <div
+    className={`ticket ticket${state ? '-' + state : ''}`}
+    onMouseEnter={toggleState(true)}
+    onMouseLeave={toggleState(false)}
+  >
     <EmptyCheckboxIcon />
-    <StarIcon />
-    {/* {ticket.assignedTo ? (
-      <img className="ticket__image" src={ticket.assignedTo.image_url} />
-    ) : ( */}
-    {/* <div className="ticket__add-button">+</div> */}
-    {/* )} */}
-    <div className="ticket__title">
-      {ticket.title}
-      <div className="ticket__info">
-        <div className="ticket__date">
-          {ticket &&
-            ticket.createdAt &&
-            moment(parseInt(ticket.createdAt, 10))
-              .fromNow()
-              .toUpperCase()}
-        </div>
-        <div className="ticket__author-wrapper">
-          Question by <span className="ticket__author">{ticket.author.username}</span>
-        </div>
-        <div className="ticket__topics">
-          {ticket.topics.map(t => (
-            <div key={t.id} className="ticket__topic">
-              {t.name}
+    <>
+      {state !== 'entered' ? (
+        <div className={`ticket__title ${state}`}>
+          {ticket.title}
+          <div className="ticket__info">
+            <div className="ticket__date">
+              {ticket &&
+                ticket.createdAt &&
+                moment(parseInt(ticket.createdAt, 10))
+                  .fromNow()
+                  .toUpperCase()}
             </div>
-          ))}
+            <div className="ticket__author-wrapper">
+              Question by <span className="ticket__author">{ticket.author.username}</span>
+            </div>
+            <div className="ticket__topics">
+              {ticket.topics.map(t => (
+                <div key={t.id} className="ticket__topic">
+                  {t.name}
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <StarIcon />
+      )}
+    </>
   </div>
 )
 
-export default Ticket
+const Fade = ({ ticket }) => {
+  const [state, setState] = useState(false)
+
+  const toggleState = val => _ => setState(val)
+
+  return (
+    <Transition in={state} timeout={150}>
+      {state => <Ticket state={state} ticket={ticket} toggleState={toggleState} />}
+    </Transition>
+  )
+}
+
+export default Fade
